48d418b7196eb7a256148107caf81ea03537af4c
raulcadierno <cadiernoraul@gmail.com>
2026-08-26 18:16:36 +0200

    creados los pasos para la prueba del escenario suma de dos valores

---
+++ features/step_definitions/calculadora_steps.js
@@ -0,0 +1,18 @@
+const { Given, When, Then } = require('@cucumber/cucumber');
+const assert = require('assert');
+const Calculadora = require('../../src/calculadora');
+
+let calc;
+let resultado;
+
+Given('que tengo una calculadora', function () {
+  calc = new Calculadora();
+});
+
+When('sumo {int} y {int}', function (n1, n2) {
+  resultado = calc.sumar(n1, n2);
+});
+
+Then('el resultado de la suma debe ser {int}', function (esperado) {
+  assert.strictEqual(resultado, esperado);
+});
